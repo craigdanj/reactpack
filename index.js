fa6a80a9ee9a0a63df4ecb6836010d25dad7baaf
Craig <craigdanj@gmail.com>
2020-01-17 22:53:09 +0530

    commented code removed

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 // In the main process.
 const { app, BrowserWindow } = require('electron')
 
-// // Or use `remote` from the renderer process.
-// // const { BrowserWindow } = require('electron').remote
-
-// let win = new BrowserWindow({ width: 800, height: 600 })
-// win.on('closed', () => {
-//   win = null
-// })
-
-// // Load a remote URL
-// win.loadURL('http://localhost:8080')
-
-// // Or load a local HTML file
-// // win.loadURL(`file://${__dirname}/app/index.html`)
-
-
 function createWindow () {
   // Create the browser window.
   let win = new BrowserWindow({
