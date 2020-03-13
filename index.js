b805f510bbf25b5ba57290296188744dcc7dba79
Craig <craigdanj@gmail.com>
2020-03-14 00:36:58 +0530

    building for mac working

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
