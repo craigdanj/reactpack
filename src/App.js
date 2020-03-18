46cbaa600113402bb4b15af590706117552f2539
Craig <craigdanj@gmail.com>
2020-03-18 22:01:27 +0530

    Demo to show how code splitting can be done added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import style from "./style.less";
 
 const App = () => {
+
+	//Lazy loading a module can be done with the use of dynamic imports as shown below.
+	// import('modulePathHere').then(({ default: export })=> {
+	// 	console.log(export);
+	// });
+
+	//Lazy loading a component or dynamically importing a component can be done as follows.
+	const Test = React.lazy(() => import('./Test'))
+
 	return (
 		<div className={style.header}>
 			<p>React app!</p>
+			<Suspense fallback={<p>Loading</p>}>
+				<Test />
+			</Suspense>
 		</div>
 	);
 };
